5594676812cea1645cc54d4aa7fa936f844d7bf7
vladlvov <105988540+vladlvov@users.noreply.github.com>
2024-07-31 22:56:20 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Твой цвет любимый: ')
+print(my_string.upper())
+print(my_string. lower())
+print(my_string. replace(' ', ''))
+print(my_string. capitalize())
+print('Твой цвет любимый:'.replace("Твой" , 'Мой'))
+print('Твой цвет любимый:'.replace('любимый','не любимый '))
+
